701345db044203a5329c4af904ffbfdd472ea62d
rajeshwar3 <132666971+rajeshwar3@users.noreply.github.com>
2023-12-09 00:35:48 +0530

    Download Counts

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark")
 })
+
+let totalCount = localStorage.getItem('totalCount');
+if (!totalCount) {
+  localStorage.setItem('totalCount', '0');
+  totalCount = 0;
+}
+
+document.getElementById('totalCount').textContent = totalCount;
+
+function updateCount() {
+  totalCount++;
+  localStorage.setItem('totalCount', totalCount);
+  document.getElementById('totalCount').textContent = totalCount;
+}
+
+document.getElementById('button1').addEventListener('click', updateCount);
+
+document.getElementById('button2').addEventListener('click', updateCount);
